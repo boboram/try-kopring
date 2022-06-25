b7d33a27db9d53a5802473b2fd7a8960c8fd3595
boram <5018b@naver.com>
2022-06-25 11:23:13 +0900

    README.md 수정, 테스트 샘플 생성

---
+++ src/test/kotlin/com/boram/trykopring/TestExample.kt
@@ -0,0 +1,17 @@
+package com.boram.trykopring
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TestExample {
+
+    @Test
+    fun testHelloWorld() {
+        println("hello world~")
+    }
+
+    @Test
+    fun testEqualsText() {
+        assertEquals("test", "test")
+    }
+}
