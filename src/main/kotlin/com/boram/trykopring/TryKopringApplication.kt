60043df13a988a39b8716fab1463a4404c5405c5
boram <5018b@naver.com>
2022-06-25 11:19:09 +0900

    project init

---
+++ src/main/kotlin/com/boram/trykopring/TryKopringApplication.kt
@@ -0,0 +1,11 @@
+package com.boram.trykopring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TryKopringApplication
+
+fun main(args: Array<String>) {
+	runApplication<TryKopringApplication>(*args)
+}
---
+++ src/test/kotlin/com/boram/trykopring/TryKopringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.boram.trykopring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TryKopringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
