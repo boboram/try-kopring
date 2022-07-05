f105c301763c7b315231fb15acea53c5d4477e0c
boram <5018b@naver.com>
2022-07-05 20:24:24 +0900

    README.md 포트 죽이기 추가

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 - 코틀린 스프링부트로 이것저것 해보기
 
+# kill port
+- `lsof -i :0625`
+- `kill -9 #pid#`
 # 랜덤한 명언 api 호출 
 - `/wise/advice`
 - `rest api`
@@ -10,5 +13,6 @@
 # 스콘 정보 크롤링 해보기
 - Jsoup 라이브러리 사용하기
 - `scone/crawling`
+  - 네이버 자체에서 막은건지 5개만 불러와진다. 네이버 쇼핑 검색 API를 쓰면 된다고 한다. 
 - 이미지 
 ![img.png](img.png)
--- src/main/kotlin/com/boram/trykopring/web/SconeCrawlingController.kt
+++ src/main/kotlin/com/boram/trykopring/web/SconeCrawlingController.kt
@@ -13,9 +13,8 @@ class SconeCrawlingController {
     @RequestMapping("/crawling")
     fun list(): String {
 
-        val keyword = "비건 스콘"
-        val searchSite =
-            "https://search.shopping.naver.com/search/all?frm=NVSCPRO&origQuery=$keyword&pagingIndex=1&pagingSize=40&productSet=total&query=$keyword&sort=review&timestamp=&viewType=list"
+        val keyword = "비건스콘"
+        val searchSite = "https://search.shopping.naver.com/search/all?frm=NVSCPRO&origQuery=$keyword&pagingIndex=1&pagingSize=90&productSet=total&query=$keyword&sort=review&timestamp=&viewType=list"
         val doc = Jsoup.connect(searchSite + keyword).get()
 
         var res = """
@@ -29,7 +28,7 @@ class SconeCrawlingController {
             val href = titleEl.attr("href")
             val price = it.select(".basicList_price_area__1UXXR > .basicList_price__2r23_ > span > .price_num__2WUXn").text()
 
-            res += """
+            val itemRes = """
                 [$title]
                 주소: [$href]
                 가격: $price
@@ -37,7 +36,9 @@ class SconeCrawlingController {
                 
             """.trimIndent()
 
-            println(res)
+            res += itemRes
+
+            println(itemRes)
 
         }
 
